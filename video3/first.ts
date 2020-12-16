ebfdb3169bcdfcdf3e7be289810d88f11568991f
ronit-mukherjee <connect.ronit@gmail.com>
2020-12-16 07:32:09 +0530

    Updates in Program

--- video3/first.ts
+++ video3/first.ts
@@ -1,7 +1,7 @@
 let x:number;
 let y = 0;
 
-const sum = (a:number,b:number):number=>a+b;
+const sum = (a:number,b:number):number=> a+b;
 
 x = 10;
 y = 100;
--- video3/second.ts
+++ video3/second.ts
@@ -1,7 +1,1 @@
-function multiply(a:number, b:number):number {
-    return a*b;
-}
-
-function divide(a:number,b:number){
-    return a/b;
-}
+function multiply(a:number,b:number):number{return a*b}
